001fbf315f6c4605fab32ece343e089f2d9ec234
fdarre <frederic.darre@outlook.com>
2022-11-17 14:30:48 +0100

    Adds  Will smart contract

---
+++ contracts/4_Will.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0;
+
+contract Will 
+{
+    address owner;
+    uint fortune;
+    bool isDeceased;
+
+    //payable: allow this function to send and receive ether
+    constructor() payable
+    {
+        owner = msg.sender; //msg sender represents address that is being called
+        fortune = msg.value; //msg value telle=s us how much ether is being sent
+        isDeceased = false; //when contract is initialized, grandfather is not dead yet
+    }
+
+    //create modifier so the only person who can call the contract is the owner
+    modifier onlyOwner 
+    {
+        require(msg.sender == owner);
+        _; //shift back to the actual function
+    }
+
+    modifier mustBeDeceased 
+    {
+        require(isDeceased == true);
+        _; 
+    }
+
+    //list of family wallets
+    address payable[] familyWallets;
+
+    //map through inheritance - create dictionnary key store value
+    mapping(address => uint) inheritance;
+
+    //set inheritance for each address
+    function setInheritance(address payable wallet, uint amount) public
+    {
+        
+        familyWallets.push(wallet); //add wallets to the family wallets
+        inheritance[wallet] = amount; //add the inheritance amount for the wallet
+    }
+}
