ee022758cc8fae55cd810decdb280136b2a1d0e7
fdarre <frederic.darre@outlook.com>
2022-11-21 09:26:22 +0100

    Refactor code

--- contracts/1_Will.sol
+++ contracts/1_Will.sol
@@ -1,56 +1,55 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity >=0.7.0;
+//Qd deploy and run : the value of a transaction is the amount of ether the owner will allocate to a smart contract (ex ici some à léguer)
+
+pragma solidity >=0.7.0 <0.9.0;
 
 contract Will 
 {
     address owner;
     uint fortune;
     bool isDeceased;
-
-    //payable: allow this function to send and receive ether
-    constructor() public payable 
-    {
-        owner = msg.sender; //msg sender represents address that is being called
-        fortune = msg.value; //msg value telle=s us how much ether is being sent
-        isDeceased = false; //when contract is initialized, grandfather is not dead yet
-    }
-
-    //create modifier so the only person who can call the contract is the owner
-    modifier onlyOwner 
-    {
-        require(msg.sender == owner);
-        _; //shift back to the actual function
-    }
-
-    modifier mustBeDeceased 
-    {
-        require(isDeceased == true);
-        _; 
-    }
-
-    //list of family wallets
     address payable[] familyWallets;
-
-    //map through inheritance - create dictionnary key store value
     mapping(address => uint) inheritance;
 
-    //set inheritance for each address
-    function setInheritance(address payable wallet, uint amount) public
+    constructor() payable 
     {
-        familyWallets.push(wallet); //add wallets to the family wallets
-        inheritance[wallet] = amount; //add the inheritance amount for the wallet
+        owner = msg.sender; // address that is being called
+        fortune = msg.value; // tells us how much is being sent
+        isDeceased = false;
+    }
+    
+    function addHeir(address payable heirWallet, uint inheritanceAmount) public
+    {
+        familyWallets.push(heirWallet); //add wallets to the family wallets
+        inheritance[heirWallet] = inheritanceAmount; //add the inheritance amount for the wallet
     }
 
-
-    //pay each family member based on their wallet address
-    function payout() private mustBeDeceased {
-
-        for(i=0; i < familyWallets.length; i++)
+    function payout() private mustBeDeceased
+    { 
+         // pay each family member based on their wallet address
+        for(uint i = 0; i < familyWallets.length; i++)
         {
-            //transfer the fund from the contract address to the receiver address in the list
-            familyWallets[i].transfer(inheritance[familyWallets[i]]);                   
+            familyWallets[i].transfer(inheritance[familyWallets[i]]);                
         }
-
     }
+
+    //Oracle switch simulation
+    function hasDeceased() public onlyOwner {
+        isDeceased = true;
+        payout();
+    }
+
+    //only allocate funds if grandpa is deceased
+    modifier mustBeDeceased {
+        require(isDeceased == true);
+        _;
+    }
+
+    //only person who can call the contract is the owner 
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+
 }
